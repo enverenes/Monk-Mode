934d3fcac1b24e09b9f2ad8546f6b19bf1d84dd3
zeta1399 <84230871+zeta1399@users.noreply.github.com>
2023-02-24 20:01:49 +0300

    Initial Commit

---
+++ Monk Mode/Monk_ModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monk_ModeApp.swift
+//  Monk Mode
+//
+//  Created by Enver Enes Keskin on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Monk_ModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Monk ModeUITests/Monk_ModeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Monk_ModeUITestsLaunchTests.swift
+//  Monk ModeUITests
+//
+//  Created by Enver Enes Keskin on 24.02.2023.
+//
+
+import XCTest
+
+final class Monk_ModeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
